a9b0030210cd1d8146fef2f6441d35022a8dc996
vi_acosta <55991392+VitorAcosta@users.noreply.github.com>
2022-06-05 23:39:07 -0300

    Update do teste de RG

--- src/test/java/org/fpij/jitakyoei/model/beans/RgTest.java
+++ src/test/java/org/fpij/jitakyoei/model/beans/RgTest.java
@@ -1,6 +1,6 @@
 package test.java.org.fpij.jitakyoei.model.beans;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import org.fpij.jitakyoei.model.beans.Rg;
 import org.junit.Test;
@@ -14,9 +14,8 @@ public class RgTest
         // setup inicial
         Rg rg_teste = new Rg();
         
-        rg_teste.setNumero("123456789");
+        rg_teste.setNumero("106601507");
 
-        // (29 * 7) + 1234567 deve resultar em 1234770
         assertNotEquals(rg_teste.getNumero(), "");
     }
 }
